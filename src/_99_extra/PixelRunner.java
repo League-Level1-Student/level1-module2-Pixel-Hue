eda2fc0a1109bcc5c77a9b5236d531af3fd9fe88
student <student@ip-172-31-52-53.us-west-2.compute.internal>
2021-02-04 16:55:26 -0800

    hello

--- src/_99_extra/PixelRunner.java
+++ src/_99_extra/PixelRunner.java
@@ -4,7 +4,7 @@ public class PixelRunner {
 public static void main(String[] args) {
 	PixelParty a = new PixelParty();
 	a.setColor(0, 170, 30);
-	a.drawRectangle(0, 0, 50, 50);
+	
 	a.drawRectangle(0, 50, 50, 50);
 	a.drawRectangle(0, 100, 50, 50);
 	a.drawRectangle(0, 150, 50, 50);
@@ -18,6 +18,7 @@ public static void main(String[] args) {
 	a.drawRectangle(50, 250, 50, 50);
 	a.drawRectangle(50, 300, 50, 50);
 	
+	a.drawRectangle(100, 0, 50, 50);
 	a.drawRectangle(100, 50, 50, 50);
 	a.drawRectangle(100, 100, 50, 50);
 	a.drawRectangle(100, 150, 50, 50);
@@ -32,13 +33,47 @@ public static void main(String[] args) {
 	a.drawRectangle(150, 250, 50, 50);
 	a.drawRectangle(150, 300, 50, 50);
 	
+	a.drawRectangle(200, 50, 50, 50);
+	a.drawRectangle(200, 100, 50, 50);
+	a.drawRectangle(200, 150, 50, 50);
+	a.drawRectangle(200, 200, 50, 50);
+	a.drawRectangle(200, 250, 50, 50);
+	a.drawRectangle(200, 300, 50, 50);
 	
+	a.drawRectangle(250, 0, 50, 50);
+	a.drawRectangle(250, 50, 50, 50);
+	a.drawRectangle(250, 100, 50, 50);
+	a.drawRectangle(250, 150, 50, 50);
+	a.drawRectangle(250, 200, 50, 50);
+	a.drawRectangle(250, 200, 50, 50);
+	a.drawRectangle(250, 250, 50, 50);
+	a.drawRectangle(250, 300, 50, 50);
+	
+	a.drawRectangle(300, 0, 50, 50);
+	a.drawRectangle(300, 50, 50, 50);
+	a.drawRectangle(300, 100, 50, 50);
+	a.drawRectangle(300, 150, 50, 50);
+	
+	
+	
+	a.setColor(0, 20, 140);
+	a.drawRectangle(0, 250, 50, 50);
+	a.drawRectangle(0, 300, 50, 50);
+	a.drawRectangle(0, 350, 50, 50);
+	a.drawRectangle(0, 350, 100, 50);
+	a.drawRectangle(0, 350, 150, 50);
+	a.drawRectangle(0, 350, 200, 50);
+	a.drawRectangle(0, 350, 250, 50);
+	a.drawRectangle(0, 350, 300, 50);
+	
+	
+	a.drawRectangle(300, 200, 50, 50);
 	//frog
 	
 	
 	a.displayImage();
-	a.saveImage();
 	
+	a.saveImage();
 	
 	
 }
